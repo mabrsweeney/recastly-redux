9c5c4cad8c05bf4512bf4ff7a705fb6d30d2dc03
mabrsweeney <mabrsweeney@gmail.com>
2018-04-03 19:35:46 -0700

    state is no longer handled here

--- src/components/App.js
+++ src/components/App.js
@@ -8,57 +8,41 @@ import changeVideo from '../actions/currentVideo.js';
 import changeVideoList from '../actions/videoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import store from '../store/store.js';
+import search from '../actions/search';
+import {connect} from 'react-redux';
 
-export default class App extends React.Component {
+
+
+const App = class extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      videos: [],
-      currentVideo: null
-    };
   }
 
   componentDidMount() {
-    this.getYouTubeVideos('react tutorials');
+    this.props.handleVideoListEntryTitleClick('react redux');
   }
 
-  handleVideoListEntryTitleClick(video) {
-    this.setState({currentVideo: video});
-  }
-
-  getYouTubeVideos(query) {
-    var options = {
-      key: this.props.API_KEY,
-      query: query
-    };
-
-    this.props.searchYouTube(options, (videos) =>
-      this.setState({
-        videos: videos,
-        currentVideo: videos[0]
-      })
-    );
-  }
-
-  //TODO: swap out the React components below for the container components
-  //  you wrote in the 'containers' directory.
   render() {
     return (
       <div>
-        <Nav handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
+        <Nav />
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer />
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer />
           </div>
         </div>
       </div>
     );
   }
-}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleVideoListEntryTitleClick: (query) => dispatch(search(query))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
