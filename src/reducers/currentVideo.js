b1e823db992a48d8faa39393f9f034f345fcf0c6
mabrsweeney <mabrsweeney@gmail.com>
2018-04-03 19:36:30 -0700

    removed comments

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -3,7 +3,7 @@ import Redux from 'redux';
 var currentVideoReducer = (state = null, action) => {
   switch (action.type) {
   case 'CHANGE_VIDEO':
-    return action.video || null;
+    return action.video ;
   default:
     return state;
   }
