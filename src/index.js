326097ac4f9dafb789db58f91c842c5b3c56df6f
mabrsweeney <mabrsweeney@gmail.com>
2018-04-03 19:35:14 -0700

    wrapped App in provider to access store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import App from './components/App.js';
 import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 import store from './store/store.js';
-//TODO: Import the Provider component from 'react-redux' here!
-
-//TODO: Use the Provider component to make your store available to
-//  the rest of your app.
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
-  <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
